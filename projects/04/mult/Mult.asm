33ebd1f297a20e3424af50714584b71c9cf67cc5
psi <x.xu@hotmail.com>
2021-12-12 16:03:36 +0800

    multiplication

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,48 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+    
+    @i 
+    M=0     //i=0
+    @2
+    M=0     //RAM[2]=0
+
+    @0
+    D=M
+    @ZERO
+    D;JEQ
+
+    @1
+    D=M
+    @ZERO
+    D;JEQ
+
+(LOOP)
+    @1
+    D=M  
+    @i
+    D=M-D
+    @END
+    D;JEQ  
+
+    @0
+    D=M     //D=RAM[0]
+    @2
+    M=M+D   //RAM[2]=RAM[2]+RAM[0]
+    @i
+    M=M+1   //i=i+1
+
+ 
+    @LOOP
+    0;JMP
+
+(ZERO)
+    @2
+    M=0
+    @END
+    0;JMP
+
+(END)
+    @END
+    0;JMP
